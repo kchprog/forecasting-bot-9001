303452bcae20f8e74dc2c3885fd09863cbbd6827
Alex Chen <achen.phi@gmail.com>
2023-01-14 18:08:43 -0500

    Fixed output to be the name of the item

--- processing_testing.py
+++ processing_testing.py
@@ -1,16 +1,45 @@
 # from UserModel import _____
 # ONCE KEVIN MAKES IT A FUNCTION ADD AS IMPORT AND REMOVE recommend_product FUNCTION FROM THIS FILE
-from InputProcessor import process_input
+# from InputProcessor import process_input
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
+
+#
+
+# def process_input(filename) -> list[list[str]]:
+#     file = open(filename, "r")
+#     file = file.read()
+#     file = file.lower()
+#     products = file.split("\n\n")
+#     product_list: list[list[str]] = [['id', 'asin', 'title', 'group', 'salesrank', 'similar', 'categories', 'total_reviews', 'avg_rating']]
+#     for product in products:
+#         if 'title' in product:
+#             lines = product.split('\n')
+#             id = lines[0].split()[-1]
+#             asin = lines[1].split()[-1]
+#             title = lines[2].split('title: ')[-1]
+#             group = lines[3].split()[-1]
+#             salesrank = lines[4].split()[-1]
+#             similar = lines[5].split()[1]
+#             category_count = int(lines[6].split()[-1])
+#             categories = set()
+#             for i in range(category_count):
+#                 for category_raw in lines[7 + i].split('|'):
+#                     category = category_raw.split('[')[0]
+#                     if not category.isspace():
+#                         categories.add(category)
+#             total_reviews = lines[7 + category_count].split()[2]
+#             avg_rating = lines[7 + category_count].split()[7]
+#             product_list.append([id, asin, title, group, salesrank, similar, categories, total_reviews, avg_rating])
+#     return product_list
 
 def dictionarize(file_read_output):
     list_of_dict = []
     file_read_output.pop(0)
-    for i in file_read_output:
+    for item in file_read_output:
         dict = {}
-        dict['id'] = i[1]
-        dict['tags'] = list(i[6])
+        dict['id'] = item[1]
+        dict['tags'] = list(item[6])
         list_of_dict.append(dict)
     return list_of_dict
 
@@ -49,12 +78,21 @@ if __name__ == "__main__":
 
     file_output = process_input(r"C:\Users\lpasu00555\Desktop\Daisy_Hackathon\amazon-meta-short.txt")
 
-    new_purchased_products = [{'id': '0000000000', 'tags': ['subjects', 'cooking, food & wine', 'baking', 'bread']},
-                             {'id': '1111111111', 'tags': ['subjects', 'cooking, food & wine', 'baking', 'bread']},
-                             {'id': '2222222222', 'tags': ['subjects', 'cooking, food & wine', 'baking', 'bread']}]
+    new_purchased_products = [{'id': '0000000000', 'tags': ['christianity', 'subjects', 'sermons', 'clergy', 'books', 'preaching', 'religion & spirituality']},
+                             {'id': '1111111111', 'tags': ['christianity', 'subjects', 'sermons', 'clergy', 'books', 'preaching', 'religion & spirituality']}]
+
     new_tagged_products = dictionarize(file_output)
 
-    print(recommend_product(new_purchased_products, new_tagged_products))
+    recs = recommend_product(new_purchased_products, new_tagged_products)
+
+    print(file_output)
+
+    for item in file_output:
+        if item[1] in recs:
+            print("I recommend: ", item[2])
+
+
+
 
 
 
